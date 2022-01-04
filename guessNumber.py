d5eba13293a3b9766b696c9914b23c7da94a6127
deanmajaya <deanmajaya@gmail.com>
2022-01-04 12:25:20 +0200

    added ability for computer to play guess game on its own and to guess the user's number

--- guessNumber.py
+++ guessNumber.py
@@ -14,4 +14,55 @@ def guess(x):
 
     print(f'You got it!!! {random_number} is the one')
 
-guess(20)
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+
+    while feedback != 'c':
+        
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+
+        feedback = input(f'{guess} is this number too high (H), too low (L) or correct (C)?').lower()
+        
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+        elif feedback == 'c':
+            print(f'Yayyyyy the computer has guessed you number {guess} correctly!!!')
+            break
+        else:
+            print('what the hell are you doing?!')
+
+
+def autoGuessGame():
+    
+    low = 1
+    high = random.randint(2, 10000)
+    number = random.randint(1, high)
+    feedback = ''
+
+    while feedback != 'correct':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+
+        if guess > number:
+            print(f'wrong guess {guess} too low')
+            high = guess - 1
+        elif  guess < number:
+            print(f'wrong guess {guess} too high')
+            low = guess + 1
+        elif  guess == number:
+            feedback = 'correct'
+            print(f'thats right {guess} is correct!')
+
+#guess(20)
+#computer_guess(1000)
+autoGuessGame()
