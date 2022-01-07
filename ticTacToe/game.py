4a61c7d2bf817367c79d296d235bec83aecd11f3
deanmajaya <deanmajaya@gmail.com>
2022-01-07 14:58:00 +0200

    added a time delay between moves

--- ticTacToe/game.py
+++ ticTacToe/game.py
@@ -1,3 +1,4 @@
+import  time
 from player import HumanPlayer, RandomComputerPlayer
 
 class TicTacToe:
@@ -102,6 +103,9 @@ def play(game, x_player, o_player, print_game=True):
 
             # after we made our move, we need to alternate letters
             letter = 'O' if letter == 'X' else 'X'
+
+            # tiny break
+            time.sleep(0.8)
 
     if print_game:
         print('it\'s a tie!')
