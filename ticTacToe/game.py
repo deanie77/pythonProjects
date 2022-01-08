fbe06df05b995f020de4ba7dc484924278b2b8f2
deanmajaya <deanmajaya@gmail.com>
2022-01-08 12:42:04 +0200

    added genius computer vs random computer trial

--- ticTacToe/game.py
+++ ticTacToe/game.py
@@ -105,14 +105,27 @@ def play(game, x_player, o_player, print_game=True):
             letter = 'O' if letter == 'X' else 'X'
 
             # tiny break
-            time.sleep(0.8)
+            if print_game:
+                time.sleep(0.8)
 
     if print_game:
         print('it\'s a tie!')
 
 
 if __name__ == '__main__':
-    x_player = HumanPlayer('X')
-    o_player = GeniusComputerPlayer('O')
-    t = TicTacToe()
-    play(t, x_player, o_player, print_game=True)
+    x_wins = 0
+    o_wins = 0
+    ties = 0
+    for _ in range(1000):
+        x_player = RandomComputerPlayer('X')
+        o_player = GeniusComputerPlayer('O')
+        t = TicTacToe()
+        result = play(t, x_player, o_player, print_game=False)
+        if result == 'X':
+            x_wins += 1
+        elif result == 'O':
+            o_wins += 1
+        else:
+            ties += 1
+
+    print(f'After 1000 iterations, we see {x_wins} X wins, {o_wins} O wins, and {ties} ties')
